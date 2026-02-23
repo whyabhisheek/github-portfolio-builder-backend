f562d36633d54e0b043712c77e0130cbbaacfbff
whyabhisheek <abhishekmj5560@gmail.com>
2026-02-23 17:54:37 +0530

    Initial clean committ

--- app/main.py
+++ app/main.py
@@ -36,7 +36,9 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://localhost:8080",
-        "https://gitfolio-charm.lovable.app"
+        "https://gitfolio-charm.lovable.app",
+        "https://gitfolio-steel.vercel.app",
+        "https://gitfolio-g59co4vp2-abhishek-mj-s-projects.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
